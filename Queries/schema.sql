9c4b270a9e04bbbf0954514bd123987cb374b278
Emily Maynard <99676466+emaynard10@users.noreply.github.com>
2022-05-25 20:06:57 -0600

    adding queries files in folder

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -3,8 +3,10 @@ CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
-	 UNIQUE (dept_name)
+     UNIQUE (dept_name)
 );
+
+SELECT * FROM dept_emp
 
 CREATE TABLE employees (
 	 emp_no INT NOT NULL,
@@ -31,27 +33,26 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES titles (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
-
-CREATE TABLE dept_emp (
-  emp_no INT NOT NULL,
-  dept_no VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
-);
-
-SELECT * FROM dept_emp
